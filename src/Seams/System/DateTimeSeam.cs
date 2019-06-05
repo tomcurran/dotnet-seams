0f02266da008d62ad66d37c49051203e0891eb34
Tom Curran <tom@tomcurran.org>
2019-06-05 23:07:21 +0100

    Fix some stylecop warnings

--- src/Seams/System/DateTimeSeam.cs
+++ src/Seams/System/DateTimeSeam.cs
@@ -7,6 +7,7 @@
         public DateTime Now => DateTime.Now;
 
         public DateTime UtcNow => DateTime.UtcNow;
+
         public DateTime Today => DateTime.Today;
     }
 }
--- src/Seams/System/IDateTime.cs
+++ src/Seams/System/IDateTime.cs
@@ -7,6 +7,7 @@
         DateTime Now { get; }
 
         DateTime UtcNow { get; }
+
         DateTime Today { get; }
     }
 }
